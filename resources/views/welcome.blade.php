44a7154c4290565ccd9dd9a3226f28ec8a2333e7
Ivan Radojevic <demoche@gmail.com>
2020-08-26 09:29:58 +0200

    app scaffolding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    TVLstack
                 </div>
 
                 <div class="links">
