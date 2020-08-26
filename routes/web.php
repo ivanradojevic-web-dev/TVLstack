5727d20dedf09fec87e59214bf4137737e20d3a5
Ivan Radojevic <demoche@gmail.com>
2020-08-26 16:45:41 +0200

    first test

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/{any}', 'AppController@index')->where('any', '.*')->name('app');
+Route::get('/{any}', 'AppController@index')->where('any', '.*')->middleware('auth')->name('app');
---
+++ tests/Feature/PostToTimelineTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostToTimelineTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_post_a_text_posts()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs($user = factory(\App\User::class)->create(), "api");
+
+        $response = $this->post("/api/posts",[
+            "data" => [
+                "type" => "posts",
+                "attributes" => [
+                    "body" => "Testing Body",
+                ]
+
+            ]
+        ]);
+
+        $post = \App\Post::first();
+
+        $response->assertStatus(201);
+    }
+}
