d4aeb5ec23a771b19df4fa06077e8c692422b23f
Ivan Radojevic <demoche@gmail.com>
2020-09-06 07:55:28 +0200

    FriendRequestResponseController

--- app/Http/Controllers/FriendRequestResponseController.php
+++ app/Http/Controllers/FriendRequestResponseController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Exceptions\FriendRequestNotFoundException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Friend;
+use App\Http\Resources\Friend as FriendResource;
+
 
 
 class FriendRequestResponseController extends Controller
@@ -29,4 +32,24 @@ class FriendRequestResponseController extends Controller
         ]));
 
         return new FriendResource($friendRequest);
+    }
+
+    public function destroy()
+    {
+        $data = request()->validate([
+            'user_id' => 'required',
+        ]);
+
+        try {
+            Friend::where('user_id', $data['user_id'])
+                ->where('friend_id', auth()->user()->id)
+                ->firstOrFail()
+                ->delete();
+        } catch (ModelNotFoundException $e) {
+            throw new FriendRequestNotFoundException();
+        }
+
+        return response()->json([], 204);
+    }
+
 }
